f3916ef43462fd5afaf14e510f11461f30c1900c
Rob Benney <rob.benney@gmail.com>
2022-07-13 00:51:41 -0400

    importing images for project list

---
+++ src/data.js
@@ -0,0 +1,39 @@
+import uplift from '../src/img/uplift.jpg';
+import notetaker from '../src/img/notetaker.jpg';
+import password from '../src/img/password.jpg';
+import scheduler from '../src/img/scheduler.jpg';
+import weather from '../src/img/weather.jpg';
+import factgenerator from '../src/img/factgenerator.jpg';
+
+export const products = [
+    {
+      id: 1,
+      img: uplift,
+      link: "https://uplift-srb.herokuapp.com/login",
+    },
+    {
+      id: 2,
+      img: notetaker,
+      link: "https://note-taker-rbgwu.herokuapp.com",
+    },
+    {
+      id: 3,
+      img: password,
+      link: "https://rbenney15.github.io/password-generator/",
+    },
+    {
+      id: 4,
+      img: scheduler,
+      link: "https://rbenney15.github.io/workday-scheduler/",
+    },
+    {
+      id: 5,
+      img: weather,
+      link: "https://rbenney15.github.io/weather-dashboard/",
+    },
+    {
+      id: 6,
+      img: factgenerator,
+      link: "https://samdakota.github.io/Random-Fact-Generator/",
+    },
+  ];
