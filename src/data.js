9e233b952196c47d866fe4d93f5874790de0b1fb
Rob Benney <rob.benney@gmail.com>
2023-01-12 12:50:45 -0500

    adding coding quiz to project list

--- src/data.js
+++ src/data.js
@@ -1,51 +1,56 @@
 import uplift from '../src/img/uplift.jpg';
 import notetaker from '../src/img/notetaker.jpg';
-import gathr from '../src/img/gathr.png';
+import codingquiz from '../src/img/codingquiz.png';
 import scheduler from '../src/img/scheduler.jpg';
 import weather from '../src/img/weather.jpg';
 import factgenerator from '../src/img/factgenerator.jpg';
 
 export const products = [
-    {
-      id: 1,
-      img: uplift,
-      link: "https://uplift-srb.herokuapp.com/login",
-      description: "Fitness Tracker application that allows users to join a like minded community to track and share workouts.",
-      github: "https://github.com/Rbenney15/UpLift",
-    },
-    {
-      id: 2,
-      img: notetaker,
-      link: "https://note-taker-rbgwu.herokuapp.com",
-      description: "Note taking application that provides an electronic notepad for taking and saving project notes. Notes can be deleted upon project completion.",
-      github: "https://github.com/Rbenney15/note-taker",
-    },
-    {
-      id: 3,
-      img: gathr,
-      link: "https://gathr.herokuapp.com/",
-      description: "Event Planning app to assist in organzing a gathering of friends and family.",
-      github: "https://github.com/Rbenney15/gathr",
-    },
-    {
-      id: 4,
-      img: scheduler,
-      link: "https://rbenney15.github.io/workday-scheduler/",
-      description: "An editable weekday scheduler to track daily tasks.",
-      github: "https://github.com/Rbenney15/workday-scheduler",
-    },
-    {
-      id: 5,
-      img: weather,
-      link: "https://rbenney15.github.io/weather-dashboard/",
-      description: "Weather Dashboard application where a user can search for a city they would like to know the current and upcoming 5 day forecast.",
-      github: "https://github.com/Rbenney15/weather-dashboard",
-    },
-    {
-      id: 6,
-      img: factgenerator,
-      link: "https://samdakota.github.io/Random-Fact-Generator/",
-      description: "Random Fact Generator that allows users to select a fact category to be entertained with random facts from that category.",
-      github: "https://github.com/samdakota/Random-Fact-Generator",
-    },
-  ];
+  {
+    id: 1,
+    img: uplift,
+    link: "https://uplift-srb.herokuapp.com/login",
+    description:
+      "Fitness Tracker application that allows users to join a like minded community to track and share workouts.",
+    github: "https://github.com/Rbenney15/UpLift",
+  },
+  {
+    id: 2,
+    img: notetaker,
+    link: "https://note-taker-rbgwu.herokuapp.com",
+    description:
+      "Note taking application that provides an electronic notepad for taking and saving project notes. Notes can be deleted upon project completion.",
+    github: "https://github.com/Rbenney15/note-taker",
+  },
+  {
+    id: 3,
+    img: codingquiz,
+    link: "https://rbenney15.github.io/coding-quiz/",
+    description:
+      "Timed Coding Quiz that will test your basic JavaScript knowledge.",
+    github: "https://github.com/Rbenney15/coding-quiz",
+  },
+  {
+    id: 4,
+    img: scheduler,
+    link: "https://rbenney15.github.io/workday-scheduler/",
+    description: "An editable weekday scheduler to track daily tasks.",
+    github: "https://github.com/Rbenney15/workday-scheduler",
+  },
+  {
+    id: 5,
+    img: weather,
+    link: "https://rbenney15.github.io/weather-dashboard/",
+    description:
+      "Weather Dashboard application where a user can search for a city they would like to know the current and upcoming 5 day forecast.",
+    github: "https://github.com/Rbenney15/weather-dashboard",
+  },
+  {
+    id: 6,
+    img: factgenerator,
+    link: "https://samdakota.github.io/Random-Fact-Generator/",
+    description:
+      "Random Fact Generator that allows users to select a fact category to be entertained with random facts from that category.",
+    github: "https://github.com/samdakota/Random-Fact-Generator",
+  },
+];
